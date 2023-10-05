f6bcefb0f52f0ecb288bef49ee6420fb0f75ec89
saisneha17 <141055266+saisneha17@users.noreply.github.com>
2023-10-05 13:47:27 +0530

    Update C8.js

--- Javascript/Arrays/C8.js
+++ Javascript/Arrays/C8.js
@@ -1,16 +1,16 @@
 //isArray()
 l1 = [10, 20, 30, 40, 50]
-console.log(Array.isArray(l1))
+console.log(Array.isArray(l1)) // true
 
 l2 = 10
-console.log(Array.isArray(l2))
+console.log(Array.isArray(l2)) // false
 
 l3 = "javascript"
-console.log(Array.isArray(l3))
+console.log(Array.isArray(l3)) // false
 
 l4 = ["NameOne", "NameTwo", "NameThree", "NameFour"]
-console.log(l4.length)
-console.log(l4[0])
+console.log(l4.length) // 4
+console.log(l4[0]) // NameOne
 
 l5 = "NameOne"
-console.log(l5[0])
+console.log(l5[0]) // N
