8aa09db08a5909b8b705b6da3bad99a17a287261
saisneha17 <141055266+saisneha17@users.noreply.github.com>
2023-10-05 13:39:37 +0530

    Update C7.js

--- Javascript/Arrays/C7.js
+++ Javascript/Arrays/C7.js
@@ -6,4 +6,4 @@ console.log(l1.join('')) // 1020304050
 console.log(l1.join(' ')) //10 20 30 40 50
 console.log(l1.join('-')) //10-20-30-40-50
 console.log(l1.join('=')) //10=20=30=40=50
-console.log(l1.join(' and ')) //10=20=30=40=50
+console.log(l1.join(' and ')) //10 and 20 and 30 and 40 and 50
