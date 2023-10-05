b8d021bec8f6e6453f24521f94f1698b80e1a11d
saisneha17 <141055266+saisneha17@users.noreply.github.com>
2023-10-05 13:06:34 +0530

    Update C4.js

--- Javascript/Arrays/C4.js
+++ Javascript/Arrays/C4.js
@@ -7,7 +7,7 @@ function checkValues(l1){
 }
 
 //callFunc
-console.log(l1.find(checkValues))
+console.log(l1.find(checkValues)) // 40
 
 //findIndex()
 l1 = [10, 20, 30, 40, 50]
@@ -18,4 +18,5 @@ function checkValues(l1){
 }
 
 //callFunc
-console.log(l1.findIndex(checkValues))
+console.log(l1.findIndex(checkValues)) // 40
+                                       // 3
