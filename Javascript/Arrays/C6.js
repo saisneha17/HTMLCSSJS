a02b3aed3c0453c8930c101ac4bea0a4cb3a9bb5
saisneha17 <141055266+saisneha17@users.noreply.github.com>
2023-10-05 13:34:03 +0530

    Update C6.js

--- Javascript/Arrays/C6.js
+++ Javascript/Arrays/C6.js
@@ -1,10 +1,11 @@
-//concat
+//concat (merging the arrays)
 l1 = [10, 20, 30, 40]
 l2 = [50, 60, 70, 80]
-console.log(l1)
-console.log(l2)
-console.log(l1.concat(l2))
+console.log(l1) //  [10, 20, 30, 40]
+console.log(l2) //  [50, 60, 70, 80]
+console.log(l1.concat(l2)) // [10, 20, 30, 40]
+                           // [50, 60, 70, 80]
 
-console.log(l1.concat(100, 200))
+console.log(l1.concat(100, 200)) // [10, 20, 30, 40, 100, 200]
 
-console.log(l1.concat(1000, 2000).concat(3000, 4000))
+console.log(l1.concat(1000, 2000).concat(3000, 4000)) // [10, 20, 30, 40, 1000, 2000, 3000, 4000]
